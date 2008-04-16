328f09a86b60c892554d9e48c54d5c039891243f
Noël Dellofano <ndellofano@apple.com>
2008-04-16 21:06:47 +0000

    removing zfs-102

--- build/zfs.build/Release/zfs.kext.build/DerivedSources/zfs_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char zfsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:zfs  PROJECT:zfs-" "\n"; const double zfsVersionNumber __attribute__ ((used)) = (double)0.;
--- build/zfs.build/Release/zfs_readonly.kext.build/DerivedSources/zfs.readonly_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char zfs_readonlyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:zfs.readonly  PROJECT:zfs-" "\n"; const double zfs_readonlyVersionNumber __attribute__ ((used)) = (double)0.;
