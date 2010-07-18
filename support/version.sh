a85a1f2e2b0a9f454b380d29031b579ef8c0113f
Alex Blewitt <alex.blewitt@gmail.com>
2010-07-19 00:50:02 +0100

    Allow version to have --long qualifier
    If --long is given to version.sh, then it will print out
    a long version instead of just the number. In other words:
    
    ./version.sh
    72.1.7
    ./version.sh --long
    maczfs_72-1-7-g383f329: 72.1.7

--- support/version.sh
+++ support/version.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DESCRIPTION=`git describe --tags --long --match 'maczfs*' 2> /dev/null`
-#fixme - find out how to get the right dir here
+if [ "$1" = "--long" ]
+then
+	echo -n "${DESCRIPTION}: "
+fi
 DIR=`dirname $0`
 VERSION=`echo ${DESCRIPTION} | awk -f ${DIR}/version.awk`
-echo $VERSION
+echo ${VERSION}
