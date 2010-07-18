bd0f79b78f7d450727331cd867bdedc7e8e27915
Alex Blewitt <alex.blewitt@gmail.com>
2010-07-18 12:10:17 +0100

    Don't attempt to build 10.6 code on 10.5
    It doesn't work, and we just get the same stuff again. So
    conditionally build it if we're not on 10.5, identified by
    kernel release 9.8.0.

--- support/release.sh
+++ support/release.sh
@@ -28,7 +28,13 @@ sed -i~  -e "s/0\\.0\\.0/${VERSION}/g" ${DIR}/../${BUILD}/MacZFS.pmdoc/*
 
 cd ${DIR}/..
 xcodebuild -sdk macosx10.5 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} SYMROOT=${BUILD}/${CONFIG}105 DSTROOT=${BUILD}/ZFS105 || exit 2
-xcodebuild -sdk macosx10.6 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} SYMROOT=${BUILD}/${CONFIG}106 DSTROOT=${BUILD}/ZFS106 || exit 3
+
+# No point in building on 10.5, it'll just do the same thing again
+if [ "`sysctl -b kern.osrelease`" != "9.8.0" ] 
+then
+	xcodebuild -sdk macosx10.6 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} SYMROOT=${BUILD}/${CONFIG}106 DSTROOT=${BUILD}/ZFS106 || exit 3
+fi
+
 cd ${BUILD}
 packagemaker --doc MacZFS.pmdoc --version ${VERSION} --title "Mac ZFS ${VERSION}" --out MacZFS-${VERSION}.pkg --target 10.5 || exit 4
 )
